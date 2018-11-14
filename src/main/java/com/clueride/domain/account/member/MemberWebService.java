ac5e9ff99c043ca5e0510668f0c30946012cf8e9
jett <jettmarks@gmail.com>
2018-11-13 21:35:10 -0500

    Brief test of what needs to be added to create a REST endpoint.

---
+++ src/main/java/com/clueride/domain/account/member/MemberWebService.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2018 Jett Marks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by jett on 11/12/18.
+ */
+package com.clueride.domain.account.member;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST API for Members.
+ */
+@Path("/member")
+@RequestScoped
+public class MemberWebService {
+    @Inject
+    private MemberService memberService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Member> getAllMembers() {
+        return memberService.getAllMembers();
+    }
+}
